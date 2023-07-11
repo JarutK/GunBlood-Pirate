5593d821e38db8cb755273f787ae772fbaa5ff91
JarutK <75347790+JarutK@users.noreply.github.com>
2023-07-11 16:59:15 +0700

    Minimum Require

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,108 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public enum GameState
+{
+    Idle,
+    Start,
+    Play,
+    End
+}
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public GameState gameState;
+
+    [SerializeField] private GameObject player1;
+    [SerializeField] private GameObject player2;
+
+    [SerializeField] private TextMeshProUGUI announceText;
+    [SerializeField] private float gameStartCountDown;
+    private float gameStartTimer;
+
+    private void Awake()
+    {
+        if (instance)
+            Destroy(instance);
+        instance = this;
+    }
+
+    private void Start()
+    {
+        Initialize();
+    }
+
+    private void Initialize()
+    {
+        gameState = GameState.Idle;
+        gameStartTimer = gameStartCountDown;
+    }
+
+    private void Update()
+    {
+        GetInput();
+        CheckState();
+    }
+
+    private void CheckState()
+    {
+        switch (gameState)
+        {
+            case GameState.Idle:
+                break;
+
+            case GameState.Start:
+                CountdownToPlay();
+                break;
+
+            case GameState.Play:
+                break;
+
+            case GameState.End:
+                GameOver();
+                break;
+        }
+    }
+
+    private void GetInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (gameState == GameState.Idle)
+            {
+                announceText.gameObject.SetActive(true);
+                gameState = GameState.Start;
+            }
+            else if (gameState == GameState.End)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+        }
+    }
+
+    private void CountdownToPlay()
+    {
+        gameStartTimer -= Time.deltaTime;
+        announceText.text = gameStartTimer.ToString("0");
+
+        if (Mathf.RoundToInt(gameStartTimer) <= 0)
+        {
+            announceText.gameObject.SetActive(false);
+            gameState = GameState.Play;
+        }
+    }
+
+    private void GameOver()
+    {
+        announceText.gameObject.SetActive(true);
+
+        if (!player1 && !player2)
+            announceText.text = "DRAW";
+        else if (!player2)
+            announceText.text = "P1 WIN!";
+        else if (!player1)
+            announceText.text = "P2 WIN!";
+
+    }
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+using TMPro;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private GameObject bulletPref;
+    [SerializeField] private Transform shootPoint;
+    [SerializeField] private KeyCode shootKey;
+
+    [Header("Stat")]
+    [SerializeField] private int lifePoint = 1;
+    [SerializeField] private int ammo = 1;
+
+    private Animator anim => GetComponentInChildren<Animator>();
+    private GameManager gameManager => GameManager.instance;
+
+    private void Update()
+    {
+        if (gameManager.gameState == GameState.Play)
+            GetInput();
+    }
+
+    private void GetInput()
+    {
+        if (Input.GetKeyDown(shootKey))
+        {
+            ShootBullet();
+        }
+    }
+
+    private void ShootBullet()
+    {
+        if (ammo <= 0) return;
+
+        anim.SetTrigger("Shoot");
+        Instantiate(bulletPref, shootPoint.position, shootPoint.rotation);
+        ammo--;
+    }
+
+    private void TakeDamage()
+    {
+        lifePoint--;
+
+        if (lifePoint <= 0)
+            Die();
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+        gameManager.gameState = GameState.End;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<Bullet>())
+        {
+            TakeDamage();
+            Destroy(collision.gameObject);
+        }
+    }
+}
